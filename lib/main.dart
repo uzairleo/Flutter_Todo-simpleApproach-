bd8567a7f7724d1b7de78c4426c32008e4d9ea59
Uzair Leo <uzair.jan336@gmail.com>
2020-03-17 22:24:54 +0500

    add simple remove function that will remove the item of type string from the list

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main()
-{
+void main() {
   runApp(new FlutterTodo());
 }
 
@@ -22,16 +21,68 @@ class TodoBody extends StatefulWidget {
 }
 
 class _TodoBodyState extends State<TodoBody> {
+  //so this is the list which is the main thind in todo list each of our item todo item is stored here in the list if we want to add more we
+  //add more to this list or wanna to remove then we are going to remove our items from this list
+  List<String> todoList = [];
+  int index;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title:Text("FLutter_Todo"),
-         ),
-         body: null,
-         floatingActionButton: FloatingActionButton(
-           onPressed: null,
-           child: Icon(Icons.add),),
+        appBar: AppBar(
+          title: Text("FLutter_Todo"),
+        ),
+        body: buildTodoBody(),
+        floatingActionButton:
+            Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right:8.0),
+            child: FloatingActionButton(
+              onPressed: (){
+                removeTodoList(index);
+              },
+              child: Icon(Icons.remove),
+              tooltip: "Remove item",
+            ),
+          ),
+          
+          FloatingActionButton(
+            onPressed: addTodoList,
+            child: Icon(Icons.add),
+            tooltip: "add item",
+          ),
+        ]));
+  }
+
+  Widget buildTodoBody() {
+    return ListView.builder(
+        // itemCount: 2,
+        itemBuilder: (context, index) {
+      if (index < todoList.length) {
+        return buildListTile(todoList[index],index);
+      }
+    });
+  }
+
+  Widget buildListTile(String title,int index) {
+    this.index=index;
+    return ListTile(
+      title: Text(title),
+      onTap: (){
+        removeTodoList(index);
+      },
     );
+  }
+
+  void addTodoList() {
+    setState(() {
+      int index = todoList.length;
+      todoList.add("uzairleoTodo" + index.toString());
+    });
+  }
+  void removeTodoList(int index)
+  {
+    setState((){
+      todoList.removeAt(index);
+    });
   }
 }
